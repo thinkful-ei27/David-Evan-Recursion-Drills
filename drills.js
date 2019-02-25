d0b0debc89a1257c3ad51b5dbb42966368b5ee8d
Evan Guirino <etg624@gmail.com>
2019-02-25 09:15:16 -0800

    array double

--- drills.js
+++ drills.js
@@ -1,12 +1,19 @@
-let countSheep = function (sheep, ) {
+// let countSheep = function(sheep) {
+//   if (sheep === 0) {
+//     return;
+//   }
 
-  if (sheep === 0) {
-    return 
+//   console.log(` ${sheep} - Another sheep jumped over the fence`);
+
+//   countSheep(sheep - 1);
+// };
+// countSheep(5);
+
+const arrDouble = arr => {
+  if (!arr.length) {
+    return [];
   }
 
-  console.log(` ${sheep} - Another sheep jumped over the fence`)
-
-  countSheep(sheep-1);
-
-}
-countSheep(5);
+  return [arr[0] * 2, ...arrDouble(arr.slice(1))];
+};
+console.log(arrDouble([1, 2, 3]));
